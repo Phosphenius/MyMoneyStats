3c4d4cd320ef0c46b27d374ac223383f67add714
luca <luca.kredel@web.de>
2019-04-01 11:09:52 +0200

    Add date column to entry list component

--- client/src/components/EntryList.vue
+++ client/src/components/EntryList.vue
@@ -9,6 +9,7 @@
         <md-table-cell md-label="ID" md-sort-by="id">{{ item.id }}</md-table-cell>
         <md-table-cell md-label="Amount" md-sort-by="amount">{{ item.amount }}</md-table-cell>
         <md-table-cell md-label="Title" md-sort-by="title">{{ item.title }}</md-table-cell>
+        <md-table-cell md-label="Date" md-sort-by="date">{{ item.date }}</md-table-cell>
       </md-table-row>
     </md-table>
   </div>
